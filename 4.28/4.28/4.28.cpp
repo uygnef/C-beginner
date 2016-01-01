39691fb0336590b3e3b413defefb0113bd65e0cc
Feng Yu <uygnef@live.com>
2016-01-01 22:23:58 +0800

    4.28 4.29

---
+++ 4.28/4.28/4.28.cpp
@@ -0,0 +1,18 @@
+// 4.28.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+#include "string"
+#include"vector"
+using namespace std;
+
+
+int main()
+{
+	vector<int> mony{ 1,22,3,25,46,5 };
+	cout << sizeof(double) << sizeof(char) << sizeof mony;
+	int s; cin >> s;
+	return 0;
+}
+
